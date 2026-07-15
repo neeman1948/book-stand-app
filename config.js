f8e41b999958e5e5e490732fa76b7ff29d59b97d
neeman1948 <neeman1948@gmail.com>
2026-07-15 22:37:10 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+window.BOOK_STAND_CONFIG = {
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  adminPin: "1234",
+  tables: {
+    books: "books",
+    orders: "book_orders",
+    settings: "settings",
+    sales: "sales"
+  }
+};
